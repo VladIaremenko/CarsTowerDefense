fe05c32ae13c73a954d799d222a7476f1d2b0af1
Vlad <vlad.iaremenko@gmail.com>
2023-02-05 00:03:11 +0300

    Game updates

--- Assets/Scripts/ProjectileLogic/CannonProjectileParabolaController.cs
+++ Assets/Scripts/ProjectileLogic/CannonProjectileParabolaController.cs
@@ -4,9 +4,16 @@ namespace Assets.Scripts.ProjectileLogic
 {
     public class CannonProjectileParabolaController : ProjectileController
     {
+        [SerializeField] private Rigidbody _rigidbody;
+
+        private void OnEnable()
+        {
+            _rigidbody.AddForce(transform.forward * 10, ForceMode.Impulse);
+        }
+
         public override void Move()
         {
-            Debug.Log("Fall");
+           
         }
     }
 }
